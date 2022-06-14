5460b83ec79415d24fb78601784290f4ca428ba0
PostEx-Taimoor Butt <taimoor.ahmad@postex.pk>
2022-06-14 21:41:36 +0500

    last commit

---
+++ JenkinsTest/src/main/java/com/test/jenkins/JenkinsTest.java
@@ -0,0 +1,7 @@
+package com.test.jenkins;
+
+public class JenkinsTest {
+    public static void main(String[] args){
+        System.out.println("This world is full of wonders and beautiful things");
+    }
+}
--- src/JenkinsTestFile.java
+++
@@ -1,5 +0,0 @@
-public class JenkinsTestFile {
-    public static void main(String[] args) {
-        System.out.println("This is the beginning of new era");
-    }
-}
