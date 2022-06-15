95a16e9c235be7716ac9f219d22ff0f8ecf148f6
PostEx-Taimoor Butt <taimoor.ahmad@postex.pk>
2022-06-15 16:18:27 +0500

    last commit

--- JenkinsTest/src/main/java/com/test/jenkins/JenkinsTest.java
+++ JenkinsTest/src/main/java/com/test/jenkins/JenkinsTest.java
@@ -3,5 +3,8 @@ package com.test.jenkins;
 public class JenkinsTest {
     public static void main(String[] args){
         System.out.println("This world is full of wonders and beautiful things");
+        String number = "011";
+        String numbertwo = "00"+(Integer.parseInt(number)+1);
+        System.out.println(numbertwo);
     }
 }
